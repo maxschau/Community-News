89bc22c2d68bf3c87246ccf065fe2ad65293d9ba
maxts <max.torre.schau@gmail.com>
2019-11-15 14:48:26 +0100

    Added two tests

--- server/tests/nyhetssakdao.test.js
+++ server/tests/nyhetssakdao.test.js
@@ -77,7 +77,29 @@ test("test that we create a new article", done => {
       overskrift : "Ny overskrift", ingress: "ny ingress", innhold: "ny innhold", bilde: "dummylink.no/", kategori: 2, viktighet: 1, forfatter: "max"
     }, callback
   );
+});
+
+test("test that we get all articles from one kategori", done => {
+  function callback(status, data) {
+    console.log("Test callback: status = " + status + ", data= " + JSON.stringify(data));
+    expect(data.length).toBe(2)
+  }
+  nyhetssakDao.getArtiklerByCategory(2, callback);
 })
+
+test("test that we can delete one article", done => {
+  function callback(status, data) {
+    console.log("Test callback: status = " + status + ", data= " + JSON.stringify(data));
+    let amount = -1;
+    nyhetssakDao.getAll((data) => {
+        amount = data.length;
+    });
+    expect(length).toBeLessThan(4);
+    done();
+  }
+  nyhetssakDao.deleteOne(1, callback);
+})
+
 
 
 afterAll(() => {

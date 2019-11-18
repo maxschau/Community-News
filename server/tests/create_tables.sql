2d982a2ef9892fae63ce64e6a0cbeea02a723301
maxts <max.torre.schau@gmail.com>
2019-11-18 16:09:16 +0100

    Small change in test

--- server/tests/create_tables.sql
+++ server/tests/create_tables.sql
@@ -19,5 +19,5 @@ CREATE TABLE article (
     author varchar(30) NOT NULL,
     likes int(11) NOT NULL DEFAULT '0',
     PRIMARY KEY(id),
-    FOREIGN KEY(categories) REFERENCES categories(id)
+    FOREIGN KEY(category) REFERENCES categories(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
